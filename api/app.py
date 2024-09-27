5b1c5ae81fa678297dcbcda18a4b3a2b25d5b3f3
FlixGo <92895161+TioFlavin@users.noreply.github.com>
2024-09-27 13:52:25 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,34 +1,44 @@
-from flask import Flask, jsonify
-from googleapiclient.discovery import build
+from flask import Flask, request, jsonify
+import requests
+import re
 
 app = Flask(__name__)
 
 API_KEY = 'AIzaSyDROWO5FOF3p7JY4Ux58oI8sPsXJfX6jBI'
-youtube = build('youtube', 'v3', developerKey=API_KEY)
 
-@app.route('/youtube/shots', methods=['GET'])
-def get_youtube_shots():
-    request = youtube.search().list(
-        part='snippet',
-        maxResults=100,
-        q='shorts',
-        type='video',
-        videoDuration='short',
-        regionCode='BR'  # Filtra para o Brasil
-    )
-    response = request.execute()
-    
-    shots = []
-    for item in response.get('items', []):
-        video_id = item['id']['videoId']
-        shots.append({
-            'titulo': item['snippet']['title'],
-            'videoId': video_id,
-            'link': f'https://www.youtube.com/watch?v={video_id}',  # Link do vídeo
-            'thumbnail': item['snippet']['thumbnails']['default']['url']
-        })
-    
-    return jsonify(shots)
+def extract_playlist_id(url):
+    match = re.search(r'list=([a-zA-Z0-9_-]+)', url)
+    return match.group(1) if match else None
+
+@app.route('/playlist', methods=['GET'])
+def get_playlist_videos():
+    url = request.args.get('link')
+    playlist_id = extract_playlist_id(url)
+
+    if not playlist_id:
+        return jsonify({'error': 'Invalid playlist URL'}), 400
+
+    videos = []
+    next_page_token = None
+
+    while True:
+        api_url = f'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={playlist_id}&key={API_KEY}&maxResults=50'
+        if next_page_token:
+            api_url += f'&pageToken={next_page_token}'
+
+        response = requests.get(api_url)
+        data = response.json()
+
+        for item in data.get('items', []):
+            title = item['snippet']['title']
+            video_url = f"https://www.youtube.com/watch?v={item['snippet']['resourceId']['videoId']}"
+            videos.append({'title': title, 'url': video_url})
+
+        next_page_token = data.get('nextPageToken')
+        if not next_page_token:
+            break
+
+    return jsonify(videos)
 
 if __name__ == '__main__':
     app.run(debug=True)
