647b44cd274303548d7c4d8d6b67b965e51155fd
FlixGo <92895161+TioFlavin@users.noreply.github.com>
2024-11-05 22:25:59 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -220,7 +220,13 @@ def set_webhook():
     bot.set_webhook(url=webhook_url)
 
 # Função principal para rodar o servidor Flask
-if __name__ == '__main__':
-    set_webhook()
-    app.run()
-    
+# Função para lidar com as atualizações do Telegram
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return 'OK', 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
